047ce4112a35391e2ddd9083cfa21196ae87fbb7
Arnav Gupta <info@arnavgupta.net>
2021-03-24 12:00:36 +0530

    changes

---
+++ pages/api/auth/login.js
@@ -0,0 +1,34 @@
+import { connectToDatabase } from "../../../util/mongodb";
+import bcrypt from "bcrypt";
+export default async (req, res) => {
+  if (req.method == "POST") {
+    const { db } = await connectToDatabase();
+    let user = await db
+      .collection("userData")
+      .find({ username: req.body.username })
+      .limit(1)
+      .count();
+    let userData = await db
+      .collection("userData")
+      .find({ username: req.body.username })
+      .limit(1)
+      .toArray();
+    if (user == 0) {
+      res.status(202).send("username");
+    } else {
+      bcrypt.compare(
+        req.body.password,
+        userData[0].password,
+        function (err, result) {
+          if (result) {
+            res.status(200).send(userData);
+          } else {
+            res.status(202).send("password");
+          }
+        }
+      );
+    }
+  } else {
+    res.status(202).json("error");
+  }
+};
---
+++ pages/api/auth/register.js
@@ -0,0 +1,42 @@
+import { connectToDatabase } from "../../../util/mongodb";
+import bcrypt from "bcrypt";
+export default async (req, res) => {
+  if (req.method == "POST") {
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+      req.body.password = hash;
+    });
+    const { db } = await connectToDatabase();
+    let username = await db
+      .collection("userData")
+      .find({ username: req.body.username })
+      .limit(1)
+      .count();
+    if (username == 0) {
+      let email = await db
+        .collection("userData")
+        .find({ email: req.body.email })
+        .limit(1)
+        .count();
+      if (username == 0 && email == 0) {
+        let data = await db.collection("userData").insertOne({
+          username: req.body.username,
+          email: req.body.email,
+          name: req.body.name,
+          password: req.body.password,
+        });
+        let finalData = await db
+          .collection("userData")
+          .find({ username: req.body.username })
+          .limit(1)
+          .toArray();
+        await res.status(200).json(finalData);
+      } else {
+        res.status(202).json("email exists");
+      }
+    } else {
+      res.status(202).json("username exists");
+    }
+  } else {
+    res.status(202).json("error");
+  }
+};
