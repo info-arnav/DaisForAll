83a41598cfca05c5627c73a5dbf225df30846a5a
Arnav Gupta <60637717+info-arnav@users.noreply.github.com>
2021-03-27 12:30:25 +0000

    url

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -6,7 +6,7 @@ export default function Article({ data }) {
   const description =
     "Infinity is both like a website and a diary. A place where all people across the globe get a chance to put their views and talent in front of everyone.";
   const title = `Infinity | ${data.title}`;
-  const url = "https://www.arnavgupta.net";
+  const url = `https://www.arnavgupta.net/article/${data.id}`
   const images = "https://www.arnavgupta.net/logo.png";
   const alts = "logo of the infinity website";
   const imagec = data.image;
