001eaef75e9257f70a4141df14c9bc03d24412dd
Arnav Gupta <info@arnavgupta.net>
2021-03-27 17:27:44 +0530

    article

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,16 +1,19 @@
 import axios from "axios";
+import { useEffect } from "react";
 import Head from "../../components/head";
-export default function Article({data}) {
+export default function Article({ data }) {
+  data = data[0];
   const description =
     "Infinity is both like a website and a diary. A place where all people across the globe get a chance to put their views and talent in front of everyone.";
-  const title = "Infinity | Live thousand lives in one world";
+  const title = `Infinity | ${data.title}`;
   const url = "https://www.arnavgupta.net";
   const images = "https://www.arnavgupta.net/logo.png";
   const alts = "logo of the infinity website";
-  const imagec = "https://www.arnavgupta.net/logo.png";
-  const altc = "logo of the infinity website";
-  const tags =
-    "blog, infinity, passionate bloggers, blogs, passionate, write, read, post, live thousand lives in one world";
+  const imagec = data.image;
+  const altc = data.imageDescription;
+  const tags = `blog, infinity, passionate bloggers, blogs, passionate, write, read, post, live thousand lives in one world, ${
+    data.title
+  }, ${data.tags && data.tags.toString()}`;
   const card = "summary_large_image";
   return (
     <div>
@@ -25,16 +28,15 @@ export default function Article({data}) {
         tags={tags}
         card={card}
       ></Head>
-      <div>{data}</div>
     </div>
   );
 }
 
-export async function getServerSideProps({params}){
-  const id = params.id
-  let data = []
-  const res =  axios.get(`/api/data/posts/${id}`).then(e => data = e)
+export async function getServerSideProps({ params }) {
+  const id = params.id;
+  let res = await fetch(`https://www.arnavgupta.net/api/data/posts/${id}`);
+  let data = await res.json();
   return {
-    props:{data}
-  }
+    props: { data },
+  };
 }
