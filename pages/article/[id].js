32cd6d254a65662352ef2641f42189ef9b795396
Arnav Gupta <60637717+info-arnav@users.noreply.github.com>
2021-03-27 16:02:13 +0000

    article head

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -3,9 +3,8 @@ import { useEffect } from "react";
 import Head from "../../components/head";
 export default function Article({ data }) {
   data = data[0];
-  const description =
-    "Infinity is both like a website and a diary. A place where all people across the globe get a chance to put their views and talent in front of everyone.";
-  const title = `Infinity | ${data.title}`;
+  const description = data.blog.replace(/<[^>]*>/g, '').splice(0, data.blog.replace(/<[^>]*>/g, '').indexOf("."))
+  const title = `Infinity | ${data.title} | ${data.username}`;
   const url = `https://www.arnavgupta.net/article/${data._id}`;
   const images = "https://www.arnavgupta.net/logo.png";
   const alts = "logo of the infinity website";
