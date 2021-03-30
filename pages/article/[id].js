5ca8d54f223a89f0d8ee3a0ded648a22a1c6d678
Arnav Gupta <info@arnavgupta.net>
2021-03-30 18:44:08 +0530

    main entity

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -64,7 +64,7 @@ export default function Article({ data }) {
               "@type": "Article",
               mainEntityOfPage: {
                 "@type": "WebPage",
-                "@id": "https://google.com/article",
+                "@id": url,
               },
               a: "",
               headline: data.title,
