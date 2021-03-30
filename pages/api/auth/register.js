2911d22817b6e01cdb4606d2eca802361ade8874
Arnav Gupta <info@arnavgupta.net>
2021-03-30 16:18:05 +0530

    hash password for sure

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -3,62 +3,62 @@ import bcrypt from "bcryptjs";
 export default async (req, res) => {
   if (req.method == "POST") {
     bcrypt.genSalt(10, function (err, salt) {
-      bcrypt.hash(req.body.password, 10, function (err, hash) {
+      bcrypt.hash(req.body.password, 10, async function (err, hash) {
         req.body.password = hash;
+        const { db } = await connectToDatabase();
+        let username = await db
+          .collection("userData")
+          .find({ username: req.body.username })
+          .limit(1)
+          .count();
+        if (username == 0) {
+          let email = await db
+            .collection("userData")
+            .find({ email: req.body.email })
+            .limit(1)
+            .count();
+          if (username == 0 && email == 0) {
+            let data = await db
+              .collection("userData")
+              .insertOne({
+                username: req.body.username,
+                email: req.body.email,
+                name: req.body.name,
+                password: req.body.password,
+                image: "",
+                images: [""],
+                dateCreated: new Date(),
+                dateUpdated: new Date(),
+                profile: "",
+                profiles: [""],
+                passwords: [req.body.password],
+                usernames: [req.body.username],
+                followers: [],
+                following: [],
+                bookmarked: [],
+                posts: [],
+                viewedPosts: [],
+                viewedProfiles: [],
+                tags: [],
+                viewedTypes: [],
+                liked: [],
+              })
+              .then((e) => {
+                return e;
+              })
+              .then((e) =>
+                res
+                  .status(200)
+                  .json([{ _id: e.ops[0]._id, username: e.ops[0].username }])
+              );
+          } else {
+            res.status(202).json("email exists");
+          }
+        } else {
+          res.status(202).json("username exists");
+        }
       });
     });
-    const { db } = await connectToDatabase();
-    let username = await db
-      .collection("userData")
-      .find({ username: req.body.username })
-      .limit(1)
-      .count();
-    if (username == 0) {
-      let email = await db
-        .collection("userData")
-        .find({ email: req.body.email })
-        .limit(1)
-        .count();
-      if (username == 0 && email == 0) {
-        let data = await db
-          .collection("userData")
-          .insertOne({
-            username: req.body.username,
-            email: req.body.email,
-            name: req.body.name,
-            password: req.body.password,
-            image: "",
-            images: [""],
-            dateCreated: new Date(),
-            dateUpdated: new Date(),
-            profile: "",
-            profiles: [""],
-            passwords: [req.body.password],
-            usernames: [req.body.username],
-            followers: [],
-            following: [],
-            bookmarked: [],
-            posts: [],
-            viewedPosts: [],
-            viewedProfiles: [],
-            tags: [],
-            viewedTypes: [],
-            liked: [],
-          })
-          .then((e) => {
-            return e;
-          })
-          .then((e) =>
-            res
-              .status(200)
-              .json([{ _id: e.ops[0]._id, username: e.ops[0].username }])
-          );
-      } else {
-        res.status(202).json("email exists");
-      }
-    } else {
-      res.status(202).json("username exists");
-    }
   } else {
     res.status(202).json("error");
   }
