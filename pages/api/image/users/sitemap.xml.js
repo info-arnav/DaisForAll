815d9edebe71d7845fec2b532ef92f2f8d7f4c38
Arnav Gupta <info@arnavgupta.net>
2021-04-04 00:44:15 +0530

    added xml extensions:

---
+++ pages/api/image/users/sitemap.xml.js
@@ -0,0 +1,55 @@
+import { connectToDatabase } from "../../../../util/mongodb";
+import xml from "xml";
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+  const users = await db
+    .collection("userData")
+    .find({})
+    .toArray()
+    .then(async (e) => {
+      let a = await e.map((e) => {
+        if (e.image) {
+          return {
+            url: [
+              { loc: `https://www.arnavgupta.net/user/${e.username}` },
+              {
+                "image:image": [
+                  {
+                    "image:loc": `https://www.arnavgupta.net/api/image/users/${e.username}`,
+                  },
+                ],
+              },
+              { lastmod: `${e.dateUpdated}` },
+            ],
+          };
+        }
+      });
+      return a;
+    })
+    .then((e) => {
+      e.push({
+        _attr: {
+          xlms: "http://www.sitemaps.org/schemas/sitemap/0.9",
+          "xmlns:image": "http://www.google.com/schemas/sitemap-image/1.1",
+        },
+      });
+      return e;
+    })
+    .then((e) =>
+      res.send(
+        xml(
+          [
+            {
+              urlset: e,
+            },
+          ],
+          {
+            declaration: {
+              standalone: "yes",
+              encoding: "UTF-16",
+            },
+          }
+        )
+      )
+    );
+};
