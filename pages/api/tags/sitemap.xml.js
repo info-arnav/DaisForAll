5f480b71552fa4a60813ed6e4c81cebe15a76a54
Arnav Gupta <info@arnavgupta.net>
2021-04-12 00:42:25 +0530

    tags sitemap

--- pages/api/tags/sitemap.xml.js
+++ pages/api/tags/sitemap.xml.js
@@ -6,9 +6,7 @@ export default async (req, res) => {
     .collection("posts")
     .aggregate([
       {
-        $project: {
-          username: 1,
-        },
+        $project: { tags: "1" },
       },
     ])
     .toArray()
