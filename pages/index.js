b43813af8ae04399888242f1b02790ec4478b09b
Arnav Gupta <60637717+info-arnav@users.noreply.github.com>
2022-08-05 01:46:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -259,11 +259,11 @@ export default function Home(data) {
             ))}
         </div>
         <br></br>
-        {data.length > 20 && (
+        {data.length > 20 ? (
           <div style={{ width: "95%", marginLeft: "calc(2.5%)" }}>
             <Button style={{ border: "none" }}>Previous</Button>{" "}
             <Button style={{ border: "none" }}>Next</Button>
-          </div>
+          </div> : <div />
         )}{" "}
       </main>
       <Footer></Footer>
